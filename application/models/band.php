95e9dfa7dee94c500fdb3f3faa64fb65bb55d475
Gavin Potts <gavin@facultycreative.com>
2012-07-26 21:37:10 -0400

    Reset query

--- application/models/band.php
+++ application/models/band.php
@@ -14,7 +14,7 @@ class Band extends CI_Model {
     
     function find_band($query){
     	$this->db->select('id, band_name');
-    	//$this->db->like('band_name', $query);
+    	$this->db->like('band_name', $query);
     	$this->db->from('bands');
     	$bands = $this->db->get();
     	return $bands->result();
